62b6eb11bbd4ea6f3f7ec1b594568a56435f99c2
cyberfined <cyberfined@protonmail.com>
2020-12-09 07:41:22 +0000

    Add /about and /about/author pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/', to: 'about#project' # temporarily set main page to about#project
+  get 'about', to: 'about#project'
+  get 'about/author'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
