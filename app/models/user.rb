9e3ff5e828a5adbfd12b39321cade8ef41629dc1
cyberfined <cyberfined@protonmail.com>
2021-01-18 13:40:09 +0000

    Add instance method to find all passed test with the same level to the User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :user_answer_mappings
   has_many :answers, through: :user_answer_mappings
   has_secure_password
+
+  def passed_tests_by_level(level)
+    questions = Question.find(answers.map(&:question_id))
+    Test.where(id: questions.map(&:test_id), level: level)
+  end
 end
