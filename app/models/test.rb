7a8aadb6bb7ef37ec6de72895a710dfd47922cef
cyberfined <cyberfined@protonmail.com>
2021-01-18 12:35:22 +0000

    Add class method to retrieve tests titles in descending order to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,10 @@
 class Test < ApplicationRecord
+  def self.tests_titles_by_category(category)
+    category = Category.find_by(title: category)
+    if category.nil?
+      []
+    else
+      Test.where(category_id: category.id).order(title: :desc).pluck(:title)
+    end
+  end
 end
