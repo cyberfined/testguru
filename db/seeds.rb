6bc99b0a3b10cb637b143f50a4db08c021ddf2ac
cyberfined <cyberfined@protonmail.com>
2021-01-19 13:49:54 +0000

    Add emptiness checking to the db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,42 @@
-users = User.create!([{ login: 'alice', password: '12345678' },
-                     { login: 'bob', password: '87654321' }])
+if User.first.nil?
+  users = User.create!([{ login: 'alice', password: '12345678' },
+                        { login: 'bob', password: '87654321' }])
+end
 
-categories = Category.create!([{ title: 'math' }, { title: 'programming' }])
+if Category.first.nil?
+  categories = Category.create!([{ title: 'math' }, { title: 'programming' }])
+end
 
-tests = Test.create!([{ title: 'arithmetic', level: 0, category_id: categories.first.id, creator_id: users.first.id },
-                     { title: 'x86_64 assembly', level: 3, category_id: categories.second.id, creator_id: users.second.id }])
+if Test.first.nil?
+  tests = Test.create!([{ title: 'arithmetic', level: 0, category_id: categories.first.id, creator_id: users.first.id },
+                        { title: 'x86_64 assembly', level: 3, category_id: categories.second.id, creator_id: users.second.id }])
+end
 
-questions = Question.create!([{ test_id: tests.first.id, statement: 'reduce 2 + 5' },
-                             { test_id: tests.first.id, statement: 'reduce 2 * 5' },
-                             { test_id: tests.first.id, statement: 'reduce 2 + 2 * 2' },
-                             { test_id: tests.second.id, statement: 'what this instruction does: xorq %rax, %rax?' },
-                             { test_id: tests.second.id, statement: 'are these instructions the same: movq %rax, %rdi and leaq (%rax), %rdi?' },
-                             { test_id: tests.second.id, statement: 'what this instruction does: cld?' }])
+if Question.first.nil?
+  questions = Question.create!([{ test_id: tests.first.id, statement: 'reduce 2 + 5' },
+                                { test_id: tests.first.id, statement: 'reduce 2 * 5' },
+                                { test_id: tests.first.id, statement: 'reduce 2 + 2 * 2' },
+                                { test_id: tests.second.id, statement: 'what this instruction does: xorq %rax, %rax?' },
+                                { test_id: tests.second.id, statement: 'are these instructions the same: movq %rax, %rdi and leaq (%rax), %rdi?' },
+                                { test_id: tests.second.id, statement: 'what this instruction does: cld?' }])
+end
 
-answers = Answer.create!([{ question_id: questions[0].id, statement: '7', correct: true },
-                         { question_id: questions[0].id, statement: '8', correct: false },
-                         { question_id: questions[1].id, statement: '7', correct: false },
-                         { question_id: questions[1].id, statement: '10', correct: true },
-                         { question_id: questions[2].id, statement: '8', correct: false },
-                         { question_id: questions[2].id, statement: '6', correct: true },
-                         { question_id: questions[3].id, statement: 'set rax to zero', correct: true },
-                         { question_id: questions[3].id, statement: 'add rax to itself', correct: false },
-                         { question_id: questions[4].id, statement: 'no, they don\'t', correct: false },
-                         { question_id: questions[4].id, statement: 'yes, they are', correct: true },
-                         { question_id: questions[5].id, statement: 'clear direction flag', correct: true },
-                         { question_id: questions[5].id, statement: 'reboot a computer', correct: false }])
+if Answer.first.nil?
+  answers = Answer.create!([{ question_id: questions[0].id, statement: '7', correct: true },
+                            { question_id: questions[0].id, statement: '8', correct: false },
+                            { question_id: questions[1].id, statement: '7', correct: false },
+                            { question_id: questions[1].id, statement: '10', correct: true },
+                            { question_id: questions[2].id, statement: '8', correct: false },
+                            { question_id: questions[2].id, statement: '6', correct: true },
+                            { question_id: questions[3].id, statement: 'set rax to zero', correct: true },
+                            { question_id: questions[3].id, statement: 'add rax to itself', correct: false },
+                            { question_id: questions[4].id, statement: 'no, they don\'t', correct: false },
+                            { question_id: questions[4].id, statement: 'yes, they are', correct: true },
+                            { question_id: questions[5].id, statement: 'clear direction flag', correct: true },
+                            { question_id: questions[5].id, statement: 'reboot a computer', correct: false }])
+end
 
-Result.create!([{ user_id: users.first.id, test_id: tests.second.id, points: 2 },
-                { user_id: users.second.id, test_id: tests.first.id, points: 3 }])
+if Result.first.nil?
+  Result.create!([{ user_id: users.first.id, test_id: tests.second.id, points: 2 },
+                  { user_id: users.second.id, test_id: tests.first.id, points: 3 }])
+end
