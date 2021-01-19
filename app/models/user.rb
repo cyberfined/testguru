39b78d369346f5dae4dcc93c5bfb02cfcd08e156
cyberfined <cyberfined@protonmail.com>
2021-01-19 13:30:49 +0000

    Change passed_tests_by_level method of User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   def passed_tests_by_level(level)
-    Test.joins('INNER JOIN questions AS Q ON Q.test_id = tests.id').
-      joins('INNER JOIN answers AS A ON A.question_id = Q.id').
-      joins('INNER JOIN user_answer_mappings AS M ON M.answer_id = A.id').
-      where('M.user_id = ? AND tests.level = ?', id, level).
-      distinct.
+    Test.joins('INNER JOIN results ON results.test_id = tests.id').
+      where('results.user_id = ? AND tests.level = ?', id, level).
       pluck(:title)
   end
 end
