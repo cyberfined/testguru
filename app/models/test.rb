a2e2dc7eb6de8310d90cc505a8d301d529671076
cyberfined <cyberfined@protonmail.com>
2021-01-20 14:53:54 +0000

    Add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :creator, class_name: 'User'
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+
   def self.tests_titles_by_category(category)
     Test.joins('INNER JOIN categories ON category_id = categories.id').
       where(categories: { title: category }).
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :results
+  has_many :tests, through: :results
+  has_many :created_tests, class_name: 'Test', foreign_key: 'creator_id'
 
   def passed_tests_by_level(level)
     Test.joins('INNER JOIN results ON results.test_id = tests.id').
