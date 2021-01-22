ba7e7bedab39e1db5cbeace33ebf551f78a432d1
cyberfined <cyberfined@protonmail.com>
2021-01-22 08:35:53 +0000

    Replace delete_all with destroy

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :creator, class_name: 'User'
+
   has_many :questions, dependent: :destroy
-  has_many :results, dependent: :delete_all
+  has_many :results, dependent: :destroy
   has_many :users, through: :results
 
   def self.tests_titles_by_category(category)
-    Test.joins(:category).where(categories: { title: category }).pluck(:title)
+    joins(:category).where(categories: { title: category }).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :results, dependent: :delete_all
+
+  has_many :results, dependent: :destroy
   has_many :tests, through: :results
   has_many :created_tests, class_name: 'Test', foreign_key: 'creator_id', dependent: :destroy
 
