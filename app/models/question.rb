b243867e38b5c06464427674d7f3a9f8b4c36a10
cyberfined <cyberfined@protonmail.com>
2021-01-28 16:00:21 +0000

    Fix number of answers validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,13 +4,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :statement, presence: true
-  validate :validate_num_answers
-
-  private
-
-  def validate_num_answers
-    unless answers.count.between?(1, 4)
-      errors.add(:answers, 'number must be greater than or equal to 1 and less than or equal to 4')
-    end
-  end
 end
