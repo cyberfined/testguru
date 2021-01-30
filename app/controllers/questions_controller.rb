1131ddd654680ff673f13dc81d1d83c4c6f85d5e
cyberfined <cyberfined@protonmail.com>
2021-01-30 16:25:49 +0000

    Change URL with URL helper

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,7 +14,7 @@ class QuestionsController < ApplicationController
   def create
     question = @test.questions.build(question_params)
     if question.save
-      redirect_to "/tests/#{@test.id}/questions"
+      redirect_to :test_questions
     else
       redirect_to :new_test_question
     end
