047613f704a23035d6c32419e04f978ac6925fe2
cyberfined <cyberfined@protonmail.com>
2021-02-20 09:52:23 +0000

    Customize standard devise views and controller to set first and last name to the user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'tests#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   resources :tests do
     member do
