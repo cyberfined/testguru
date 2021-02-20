3f31ab6f613afbbabbdb7397284bedb0a8275309
cyberfined <cyberfined@protonmail.com>
2021-02-20 23:29:09 +0000

    Change test's creation method

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -8,8 +8,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def create
-    @test = Test.new(test_params)
-    @test.creator = current_user
+    @test = current_user.created_tests.build(test_params)
     if @test.save
       redirect_to admin_test_path(@test)
     else
