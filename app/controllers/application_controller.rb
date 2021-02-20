23f58e47766d52d355841f2f8e1438edd8b61e42
cyberfined <cyberfined@protonmail.com>
2021-02-20 15:09:44 +0000

    Rewrite after_sign_in_path_for method with super calling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    return admin_tests_path if current_user.admin?
-    root_path
+    if current_user.admin?
+      admin_tests_path
+    else
+      super
+    end
   end
 end
