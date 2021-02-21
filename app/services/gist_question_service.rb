1fb2a615bd36b56a11a38a3adcdf23ddd9573877
cyberfined <cyberfined@protonmail.com>
2021-02-21 09:33:39 +0000

    Implement GistQuestionService

---
+++ app/services/gist_question_service.rb
@@ -0,0 +1,27 @@
+class GistQuestionService
+  def initialize(question, client: nil)
+    token = Rails.application.credentials.aws[:github_access_token]
+    @client = client || Octokit::Client.new(access_token: token)
+    @question = question
+    @test = @question.test
+  end
+
+  def call
+    @client.create_gist(gist_params)
+  end
+
+  private
+
+  def gist_params
+    { description: I18n.t('question-gist-description', statement: @question.statement),
+      files: { question: { content: question_content }},
+      public: false
+    }
+  end
+
+  def question_content
+    content = [@question.statement]
+    content += @question.answers.pluck(:statement)
+    content.join("\n")
+  end
+end
