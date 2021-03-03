ba8dcc18ceb37cdbea1ca7d7fdbf6262cc73b9d8
cyberfined <cyberfined@protonmail.com>
2021-03-03 21:16:58 +0300

    Use ternary operator instead of if

--- app/helpers/header_helper.rb
+++ app/helpers/header_helper.rb
@@ -1,19 +1,11 @@
 module HeaderHelper
   def welcome_link(user)
-    url = if user.admin?
-            :admin_tests
-          else
-            :tests
-          end
+    url = user.admin? :admin_tests : :tests
     link_to("Welcome, #{user.first_name} Guru", url)
   end
 
   def badges_link(user)
-    url = if user.admin?
-            :admin_badges
-          else
-            :badges
-          end
+    url = user.admin? :admin_badges : :badges
     link_to('Badges', url)
   end
 end
